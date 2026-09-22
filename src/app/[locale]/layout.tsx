a581f64ae026392d3255466fd6fc9c0b95a52910
Pavel Didenko <patriot2006_06@mail.ru>
2026-09-22 05:22:36 +0300

    refactor: remove unused text

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,9 @@
-// src/app/[locale]/layout.tsx
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { routing, type Locale } from '@/i18n/routing';
 import { TanStackProvider } from '@/providers/TansTackProvider';
 import { ErrorLayout } from '@/modules/shared/layouts/ErrorLayout';
-
-// Эта функция нужна для статической генерации страниц
-export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
-}
 
 export default async function LocaleLayout({
   children,
