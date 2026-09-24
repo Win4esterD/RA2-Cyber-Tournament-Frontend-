d7aeb488b7f62dc03d9782e03dbbaa992e71bc9c
Pavel Didenko <patriot2006_06@mail.ru>
2026-09-24 07:24:57 +0300

    feat: add show layout logic

--- src/modules/shared/layouts/MainAppLayout.tsx
+++ src/modules/shared/layouts/MainAppLayout.tsx
@@ -2,27 +2,27 @@
 import { Header } from '@/modules/shared/ui/Header/Header';
 import { SideBar } from '@/modules/shared/ui/SideBar/SideBar';
 import type { ReactNode } from 'react';
+import { usePathname } from '@/i18n/navigation';
+
+const routesWithoutMainLayout = ['/login', '/registration', '/reset-password'];
 
 type MainAppLayoutPropsType = {
   children: ReactNode;
 };
 
 export function MainAppLayout({ children }: MainAppLayoutPropsType) {
-  return (
-    <>
-      {/* <Header onLocaleChange={() => console.log('Locale changed')} />
-      <div className="flex">
-        <SideBar link="/" />
-        <main className="flex-1 overflow-y-auto">{children}</main>
-      </div> */}
+  const pathname = usePathname();
+  const showMainLayout = !routesWithoutMainLayout.includes(pathname);
 
-      <div className="flex min-h-screen">
-        <SideBar link="/" />
-        <div className="flex flex-1 flex-col">
-          <Header onLocaleChange={() => console.log('Locale changed')} />
-          <main className="flex-1">{children}</main>
-        </div>
+  return showMainLayout ? (
+    <div className="flex min-h-screen">
+      <SideBar link="/" />
+      <div className="flex flex-1 flex-col">
+        <Header onLocaleChange={() => console.log('Locale changed')} />
+        <main className="flex-1">{children}</main>
       </div>
-    </>
+    </div>
+  ) : (
+    <>{children}</>
   );
 }
